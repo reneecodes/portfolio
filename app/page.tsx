667d7a80076cb2b11d4455333974d32be7998a82
Adrian Valencia <a.valencia425@gmail.com>
2026-02-24 15:27:19 -0800

    feat: Reorder contact and form sections

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
         <Experience />
         <Skills />
         <Projects />
-        <HomepageInquiryForm />
         <Contact />
+        <HomepageInquiryForm />
       </main>
     </div>
   );
